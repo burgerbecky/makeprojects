049a4b156583c4a2f61a9d143462729ac0ed0ec0
Rebecca Heineman <becky@burgerbecky.com>
2018-05-27 19:35:06 -0700

    Tox works for Python 2.7

---
+++ unittests/test_cleanme.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Unit tests for makeproject cleanme
+
+Copyright 2013-2018 by Rebecca Ann Heineman becky@burgerbecky.com
+
+It is released under an MIT Open Source license. Please see LICENSE
+for license details. Yes, you can use it in a
+commercial title without paying anything, just give me a credit.
+Please? It's not like I'm asking you for money!
+
+"""
+
+import makeprojects
+
+########################################
+
+
+def test_truefalse():
+	pass
