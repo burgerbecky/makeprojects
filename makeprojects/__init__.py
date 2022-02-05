54af189dec281620c0c0fa1fb486abb8f5d6854c
Rebecca Heineman <becky@burgerbecky.com>
2022-02-04 20:39:07 -0600

    Updated version to 0.12.1

--- makeprojects/__init__.py
+++ makeprojects/__init__.py
@@ -53,7 +53,7 @@ from .defaults import get_configuration_settings
 ########################################
 
 ## Current version of the library as a numeric tuple
-__numversion__ = (0, 12, 0)
+__numversion__ = (0, 12, 1)
 
 ## Current version of the library
 __version__ = '.'.join([str(num) for num in __numversion__])
